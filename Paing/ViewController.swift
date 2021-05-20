f8f5f543983f531c4ebebb7b6d8c2b28317ca6b4
IamRohitSinghDhakad <rohit.dhakad@systematixindia.com>
2021-05-21 00:36:50 +0530

    First Commit
    Signed-off-by: IamRohitSinghDhakad <rohit.dhakad@systematixindia.com>

---
+++ Paing/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Paing
+//
+//  Created by Rohit Singh Dhakad on 20/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imgVwAnimation: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imgVwAnimation.backgroundColor = UIColor.blue
+        //self.view.addSubview(imgVwAnimation)
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        Up()
+
+
+    }
+
+    func bottom()  {
+
+        UIView.animate(withDuration: 3.0, delay: 0.2, options: [.curveEaseInOut], animations: {
+         //   self.imgVwAnimation.frame = CGRect(x: 0, y: 500.0, width: 300.0, height: 300.0)
+        }) { (finished) in
+            if finished {
+                // Repeat animation from bottom to top
+                //self.Up()
+            }
+        }
+    }
+
+    func Up()  {
+
+        UIView.animate(withDuration: 2.0, delay: 0, options: [.curveEaseIn], animations: {
+            self.imgVwAnimation.frame = CGRect(x: UIScreen.main.bounds.size.width*0.2, y: 0, width: 250, height: 250)
+          //  self.imgVwAnimation.frame = CGRect(x: 0, y: 0, width: 100.0, height: 100.0)
+        }) { (finished) in
+            if finished {
+                
+                self.imgVwAnimation.shake()
+                // Repeat animation to bottom to top
+               // self.bottom()
+            }
+        }
+
+    }
+
+}
+
+extension UIView {
+    func shake() {
+        self.transform = CGAffineTransform(translationX: 20, y: 0)
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
+    }
+}
---
+++ Pods/Target Support Files/Pods-Paing/Pods-Paing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Paing : NSObject
+@end
+@implementation PodsDummy_Pods_Paing
+@end
